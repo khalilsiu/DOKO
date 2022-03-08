8d1b9b8dc0998f8e9ef2ce127bc1a55834a4efca
Calvin Tang <tangkiwan@gmail.com>
2022-03-08 22:14:44 +0800

    fix: use doko backend to get dcl stats

--- client/src/store/stats/dclStatsSlice.ts
+++ client/src/store/stats/dclStatsSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import DclAPI from '../../libs/dcl-api';
+import api from '../../libs/api';
 
 export interface DclStats {
   id: number;
@@ -21,28 +21,8 @@ export interface DclStats {
 const initialState: DclStats[] = [];
 
 export const getDclStats = createAsyncThunk('DclStats/getDclStats', async () => {
-  const response = await DclAPI.get('/scenes/scene-stats.json');
-  return Object.keys(response.data).reduce((previous, key) => {
-    const landData = response.data[key];
-    const current = {
-      id: previous.length + 1,
-      coordinates: key,
-      ytdUsers: Number(landData['yesterday']['users']),
-      ytdSessions: Number(landData['yesterday']['sessions']),
-      ytdMedianSessionTime: Number(landData['yesterday']['median_session_time']),
-      ytdMaxConcurrentUsers: Number(landData['yesterday']['max_concurrent_users']),
-      weeklyUsers: Number(landData['last_7d']['users']),
-      weeklySessions: Number(landData['last_7d']['sessions']),
-      weeklyMedianSessionTime: Number(landData['last_7d']['median_session_time']),
-      weeklyMaxConcurrentUsers: Number(landData['last_7d']['max_concurrent_users']),
-      monthlyUsers: Number(landData['last_30d']['users']),
-      monthlySessions: Number(landData['last_30d']['sessions']),
-      monthlyMedianSessionTime: Number(landData['last_30d']['median_session_time']),
-      monthlyMaxConcurrentUsers: Number(landData['last_30d']['max_concurrent_users']),
-    };
-    previous.push(current);
-    return previous;
-  }, [] as DclStats[]);
+  const response = await api.get('/stats/dcl');
+  return response.data;
 });
 
 const dclStatsSlice = createSlice({
