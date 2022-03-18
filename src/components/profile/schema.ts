febfe473dd37a477193e75739fc6b0d64f589727
Anthony Siu <anthonysiu@Anthonys-MacBook-Pro.local>
2022-03-18 17:08:26 +0800

    fix: decimal-validations for tokens

--- src/components/profile/schema.ts
+++ src/components/profile/schema.ts
@@ -1,12 +1,22 @@
 import Joi from 'joi';
 import { tokens } from '../../constants/acceptedTokens';
+import { ethers } from 'ethers';
 
-const decimalValidation = (schema: Joi.NumberSchema) => {
+const decimalValidation = (schema: Joi.StringSchema) => {
   let newSchema = schema;
   for (const token of tokens) {
     newSchema = newSchema.when('rentToken', {
       is: token.symbol,
-      then: Joi.number().precision(token.decimals).positive().required(),
+      then: Joi.string().custom((value, helpers) => {
+        try {
+          ethers.utils.parseUnits(value, token.decimals);
+          return value;
+        } catch (e) {
+          console.warn(`${token.symbol} allows max decimals of ${token.decimals}`);
+          return helpers.error('number.precision', { limit: token.decimals });
+        }
+      }),
+      otherwise: Joi.string().required(),
     });
   }
   return newSchema;
@@ -16,13 +26,14 @@ export const EditLeaseSchema = {
   rentToken: Joi.string()
     .valid(...tokens.map((token) => token.symbol))
     .required(),
-  rentAmount: decimalValidation(Joi.number().positive().required()),
-  deposit: decimalValidation(Joi.number().positive().required()),
+  rentAmount: decimalValidation(Joi.string()),
+  deposit: decimalValidation(Joi.string()),
   gracePeriod: Joi.number().positive().min(7).integer().required(),
   minLeaseLength: Joi.number().min(1).integer().required(),
   maxLeaseLength: Joi.number().when('minLeaseLength', {
     is: Joi.exist(),
     then: Joi.number().greater(Joi.ref('minLeaseLength')).integer().required(),
+    otherwise: Joi.number().positive().integer().required(),
   }),
   autoRegenerate: Joi.bool().required(),
 };
